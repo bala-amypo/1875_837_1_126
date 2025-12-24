664208528e46f7072ef80ad94261a7e6edd11d55
bala-amypo <bala@amypo.in>
2025-12-24 16:41:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TierUpgradeEngineServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TierUpgradeEngineServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.entity.CustomerProfile;
+import com.example.demo.CustomerProfile;
 import com.example.demo.entity.PurchaseRecord;
 import com.example.demo.entity.TierHistoryRecord;
 import com.example.demo.entity.TierUpgradeRule;
