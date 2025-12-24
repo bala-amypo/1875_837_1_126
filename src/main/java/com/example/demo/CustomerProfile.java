5e7a0a8624fa3f847847b84092720d042a0cbdb8
bala-amypo <bala@amypo.in>
2025-12-24 08:14:58 +0000

    User commit

---
+++ src/main/java/com/example/demo/CustomerProfile.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+public class CustomerProfile {
+
+    private Long id;
+    private String customerId;
+    private String fullName;
+    private String phone;
+    private String currentTier;
+
+    public CustomerProfile() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCurrentTier() {
+        return currentTier;
+    }
+
+    public void setCurrentTier(String currentTier) {
+        this.currentTier = currentTier;
+    }
+}
