fdd58a4b72c28959e6720bb322675d4dc6131203
bala-amypo <bala@amypo.in>
2025-12-24 16:04:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/TierHistoryRecordRepository.java
+++ src/main/java/com/example/demo/repository/TierHistoryRecordRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.entity.CustomerProfile;
+import com.example.demo.CustomerProfile;
 import com.example.demo.entity.TierHistoryRecord;
 
 public interface TierHistoryRecordRepository extends JpaRepository<TierHistoryRecord, Long> {
