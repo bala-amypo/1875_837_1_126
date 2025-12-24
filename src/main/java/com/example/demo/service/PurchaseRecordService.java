1c6dea791b6bf2078cb0fa63ca09ffd3ceab4498
bala-amypo <bala@amypo.in>
2025-12-24 16:10:18 +0000

    User commit

--- src/main/java/com/example/demo/service/PurchaseRecordService.java
+++ src/main/java/com/example/demo/service/PurchaseRecordService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import java.util.List;
 
-import com.example.demo.entity.CustomerProfile;
+import com.example.demo.CustomerProfile;
 import com.example.demo.entity.PurchaseRecord;
 
 public interface PurchaseRecordService {
