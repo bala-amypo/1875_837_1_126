3870eaa9254fee0eb11a93e690f91a6224c058de
bala-amypo <bala@amypo.in>
2025-12-24 20:53:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/SimpleStatusServlet.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet(urlPatterns = "/status")
+public class SimpleStatusServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(200); resp.getWriter().write("Customer Loyalty Tier Upgrader is running");
+    }
+}
