b3aa7c5f7546b9285933edc9e7d04cd2717c3136
bala-amypo <bala@amypo.in>
2025-12-24 04:54:50 +0000

    User commit

--- src/main/java/com/example/demo/service/TierUpgradeEngineService.java
+++ src/main/java/com/example/demo/service/TierUpgradeEngineService.java
@@ -1,14 +1,9 @@
 package com.example.demo.service;
-
-import com.example.demo.model.TierHistoryRecord;
-
+import com.example.demo.entity.TierHistoryRecord;
 import java.util.List;
 
 public interface TierUpgradeEngineService {
-
     TierHistoryRecord evaluateAndUpgradeTier(Long customerId);
-
     List<TierHistoryRecord> getHistoryByCustomer(Long customerId);
-
     List<TierHistoryRecord> getAllHistory();
 }
